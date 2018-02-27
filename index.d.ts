04314d9a197130aee9de1ec8c3dfb25f4bc89ec2
MikeyBurkman <michael.burkman@gmail.com>
2018-02-27 17:02:40 -0500

    Add typescript typings

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+
+declare module 'tke-logger' {
+  import * as bunyan from 'bunyan';
+
+  interface LoggerFactory {
+    getLogger: (filename: string) => bunyan;
+  }
+
+  const instance: LoggerFactory;
+  export = instance;
+}
