9244ce5a2094b77cd036c945d2ba4c2c3d544345
MikeyBurkman <michael.burkman@gmail.com>
2018-04-05 10:15:07 -0400

    Use non-json format; no more writing to mongo

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@ declare module 'tke-logger' {
   import * as bunyan from 'bunyan';
 
   interface LoggerFactory {
-    getLogger: (filename: string) => bunyan;
+    getLogger: (filename?: string) => bunyan;
   }
 
   const instance: LoggerFactory;
